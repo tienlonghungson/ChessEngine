c0ca0fec9f1386f5da8e2960809b2c8bfdddfa24
tienlonghungson <nguyentienlong0203hungson@gmail.com>
2021-04-03 16:43:41 +0700

    reformatted

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ package src;
 
 import src.chess.Launcher;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args)
     {
         Launcher.main(args);
--- src/chess/piece/Rook.java
+++ src/chess/piece/Rook.java
@@ -7,8 +7,7 @@ import src.position.Position;
 
 import java.util.LinkedList;
 
-public class Rook extends Piece implements FirstMoveMatters
-{
+public class Rook extends Piece implements FirstMoveMatters {
     public static final int SCORE = 5;
     public static final String ID = "R";
     public static final String NAME = "Rook";
@@ -20,15 +19,13 @@ public class Rook extends Piece implements FirstMoveMatters
         this(position, isWhite, false, board);
     }
 
-    public Rook(Position position, boolean isWhite, boolean hasMoved, Board board)
-    {
+    public Rook(Position position, boolean isWhite, boolean hasMoved, Board board) {
         super(position, isWhite, board);
         this.hasMoved = hasMoved;
     }
 
     @Override
-    public LinkedList<Move> getMoves()
-    {
+    public LinkedList<Move> getMoves() {
         LinkedList<Move> moves = new LinkedList<>();
 
         getMovesHelper(1, 0, moves);
@@ -39,15 +36,12 @@ public class Rook extends Piece implements FirstMoveMatters
         return moves;
     }
 
-    private void getMovesHelper(int rowInc, int colInc, LinkedList<Move> moves)
-    {
+    private void getMovesHelper(int rowInc, int colInc, LinkedList<Move> moves) {
         Position temp = this.position.getPositionWithOffset(rowInc, colInc);
-        while(board.isInBounds(temp))
-        {
-            if(board.hasFriendlyPieceAtPosition(temp, isWhite))
+        while(board.isInBounds(temp)) {
+            if(board.hasFriendlyPieceAtPosition(temp, isWhite)) {
                 break;
-            else if(board.hasHostilePieceAtPosition(temp, isWhite))
-            {
+            } else if (board.hasHostilePieceAtPosition(temp, isWhite)) {
                 moves.add(setupMove(temp.getPositionWithOffset()));
                 break;
             }
@@ -56,46 +50,40 @@ public class Rook extends Piece implements FirstMoveMatters
         }
     }
 
-    private Move setupMove(Position position)
-    {
-        if(hasMoved)
+    private Move setupMove(Position position) {
+        if(hasMoved) {
             return new Move(this, board, position);
-        else
+        } else {
             return new FirstMove(this, board, position);
+        }
     }
 
     @Override
-    public int getScore()
-    {
+    public int getScore() {
         return SCORE;
     }
 
     @Override
-    public String getID()
-    {
+    public String getID() {
         return ID;
     }
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         return NAME;
     }
 
     @Override
-    public boolean getHasMoved()
-    {
+    public boolean getHasMoved() {
         return hasMoved;
     }
 
     @Override
-    public void setHasMoved(boolean hasMoved)
-    {
+    public void setHasMoved(boolean hasMoved) {
         this.hasMoved = hasMoved;
     }
 
-    public static Rook parseRook(String[] data, Board board)
-    {
+    public static Rook parseRook(String[] data, Board board) {
         Position position = Position.parsePosition(data[1] + data[2]);
         boolean isWhite = Boolean.parseBoolean(data[3]);
         boolean hasMoved = Boolean.parseBoolean(data[4]);
